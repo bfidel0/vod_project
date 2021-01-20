c397ea61ffd786d97e59af9a178a1f88cb34f727
bfidel0 <bfidel0@users.noreply.github.com>
2021-01-20 12:26:38 -0800

    like dislike model

--- vod_video/admin.py
+++ vod_video/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Channel, Category, VideoDetail, VideoFiles
+from .models import Channel, Category, VideoDetail, VideoFiles, ViewCount
 
 admin.site.register(Channel)
 admin.site.register(Category)
 admin.site.register(VideoDetail)
 admin.site.register(VideoFiles)
+admin.site.register(ViewCount)
--- vod_video/urls.py
+++ vod_video/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import create_channel, channel, edit_channel, upload_view, upload_processing, video_info_process, index, video_watch_view
+from .views import create_channel, channel, edit_channel, upload_view, upload_processing, video_info_process, index, video_watch_view, liked_video, dislike_video
 
 
 urlpatterns = [
@@ -7,9 +7,10 @@ urlpatterns = [
     path('watch/?v=<uuid:video_id>', video_watch_view, name='video_watch'),
     path('channel/create/', create_channel, name='create-channel'),
     path('channel/<slug>/', channel, name='mychannel'),
+    path('like/<uuid:id>/', liked_video, name='like-video'),
+    path('dislike/<uuid:id>/', dislike_video, name='dislike-video'),
     path('update_channel/<slug>/', edit_channel, name='update-channel'),
     path('upload/', upload_view, name='file-upload'),
     path('uploading/', upload_processing, name='processing'),
     path('video_detail/', video_info_process, name='video-data'),
-
 ]
